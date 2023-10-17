197567c5c6acb2629fd53a23fcadbbe0662c3177
Byeong Min Cho <merrybmc@gmail.com>
2023-10-17 13:04:25 +0900

    chore : ESLint, prettier, husky 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true
+  },
+  extends: ['react-app', 'eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
+  rules: {
+    'no-var': 'error',
+    'no-multiple-empty-lines': 'error',
+    // 'no-console': ['error', {allow: ['warn', 'error', 'info']}],
+    eqeqeq: 'error',
+    'react/jsx-pascal-case': 'error',
+    'react/jsx-key': 'error',
+    'dot-notation': 'error',
+    'linebreak-style': 0
+  }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: false,
+  bracketSameLine: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'always',
+  quoteProps: 'as-needed'
+};
