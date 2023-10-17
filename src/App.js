3682b6c4b251b3b8430328ee867c5f996a2fba78
Byeong Min Cho <merrybmc@gmail.com>
2023-10-17 12:56:34 +0900

    chore : 라이브러리 설치, 라우팅 설정, 폴더구조 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import Router from './router/Router';
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+
+body{
+  display: flex;
+  justify-content: center;
+}
+
+button{
+  cursor: pointer;
+}
+
+@font-face {
+    font-family: 'Spoqa Han Sans Neo';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2108@1.1/SpoqaHanSansNeo-Regular.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+`;
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 }
 
 export default App;
---
+++ src/router/Router.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import RandingPage from '../pages/randing/RandingPage';
+import Home from '../pages/home/Home';
+import Chat from '../pages/chat/Chat';
+import Write from '../pages/write/Write';
+import Profile from '../pages/profile/Profile';
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path='/' element={<RandingPage />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/chat' element={<Chat />} />
+      <Route path='/write' element={<Write />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
+  );
+}
