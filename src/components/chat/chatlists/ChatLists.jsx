32884b544f8a365421807f542da8bffac1642c7d
kirnjiyun <kimjiyunee@naver.com>
2023-10-19 17:07:21 +0900

    feat(채팅방) : 채팅 리스트 UI 구현 (#16)

--- src/components/chat/chatlists/ChatLists.jsx
+++ src/components/chat/chatlists/ChatLists.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import * as S from './chatlists.styled';
+import ProfileImg from '../../../images/chat/Ellipse 1.png';
 
 export default function ChatLists() {
-  return <S.Div>Chatroom</S.Div>;
+  return (
+    <S.ChatListsLayout>
+      <S.Li>
+        <S.StyledProfileImg src={ProfileImg} alt='profile' />
+        <S.UserChatBox>
+          <S.H2>애월읍 위니브 감귤농장</S.H2>
+          <S.P>이번엔 정정언제하맨마씸?</S.P>
+        </S.UserChatBox>
+        <S.Span>2020.10.13</S.Span>
+      </S.Li>
+    </S.ChatListsLayout>
+  );
 }
--- src/components/chat/chatlists/chatlists.styled.js
+++ src/components/chat/chatlists/chatlists.styled.js
@@ -1,8 +1,64 @@
 import styled from 'styled-components';
 
-export const Div = styled.div`
+const styleConstants = {
+  textColor: '#767676',
+  fontFamily: "'Spoqa Han Sans Neo', sans-serif"
+};
+export const ChatListsLayout = styled.ul`
   display: flex;
-  padding: 13px 0;
+  flex-direction: column;
+  padding-top: 24px;
+  gap: 20px;
+  min-height: 712px;
+  width: 390px;
+  align-items: center;
+  overflow-x: scroll;
+`;
 
-  border-bottom: 0.5px solid #dbdbdb;
+export const Li = styled.li`
+  width: 358px;
+  height: 42px;
+  background-color: pink;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: relative;
+`;
+
+export const StyledProfileImg = styled.img`
+  width: 42px;
+  height: 42px;
+  margin-right: 12px;
+`;
+export const UserChatBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+export const H2 = styled.p`
+  color: #000;
+  font-family: ${styleConstants.fontFamily};
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+export const P = styled.p`
+  color: ${styleConstants.textColor};
+  font-family: ${styleConstants.fontFamily};
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+export const Span = styled.span`
+  color: #dbdbdb;
+  font-family: ${styleConstants.fontFamily};
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  position: absolute;
+  bottom: 3px;
+  right: 0px;
 `;
