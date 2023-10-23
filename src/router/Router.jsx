3f31d2b1cf725ba02b9459d90cef07bb3e40dc86
esoby <141106@naver.com>
2023-10-23 13:43:20 +0900

    feat(팔로우) : 팔로우/팔로잉 페이지 route 추가 (#33)

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import RandingPage from '../pages/randing/RandingPage';
 import Home from '../pages/home/Home';
 import Chat from '../pages/chat/Chat';
 import Write from '../pages/write/Write';
 import Profile from '../pages/profile/Profile';
+import Followers from '../pages/followers/Followers';
+import Followings from '../pages/followings/Followings';
 
 export default function Router() {
   return (
@@ -14,6 +16,8 @@ export default function Router() {
       <Route path='/chat' element={<Chat />} />
       <Route path='/write' element={<Write />} />
       <Route path='/profile' element={<Profile />} />
+      <Route path='/followers' element={<Followers />} />
+      <Route path='/followings' element={<Followings />} />
     </Routes>
   );
 }
