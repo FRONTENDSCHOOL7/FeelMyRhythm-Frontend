bf1f1af4134c80a28bcdecbb20eb33726ed778a3
esoby <141106@naver.com>
2023-10-24 22:05:53 +0900

    fix(팔로잉) : 팔로워 목록 커서 포인터, 페이지 이동 추가 (#50)

--- src/components/follow/FollowList.jsx
+++ src/components/follow/FollowList.jsx
@@ -1,45 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './FollowList.styled';
+import { useNavigate } from 'react-router-dom';
 import basicProfile from '../../assets/images/profile/basic-profile-img.svg';
 
 export default function PostFollowers() {
+  const [flag, setFlag] = useState(false);
+  const navigate = useNavigate();
+
   return (
     <S.ProfileFollowersLayout>
-      <S.ProfileHeaderLayout>
-        <S.NavButton>
-          <svg xmlns='http://www.w3.org/2000/svg' width='22' height='22' viewBox='0 0 22 22' fill='none'>
-            <path d='M17.4167 11H4.58337' stroke='black' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
-            <path
-              d='M11 17.4167L4.58337 11L11 4.58334'
-              stroke='black'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            />
-          </svg>
-        </S.NavButton>
-        <S.NavContent>Followers</S.NavContent>
-      </S.ProfileHeaderLayout>
+      {/* nav bar 추가 */}
       <S.FollowerRowBox>
         <S.FollowerBox>
-          <S.FollowerImgBox>
+          <S.FollowerImgBox onClick={() => navigate('/profile')}>
             <img src={basicProfile} alt='' />
           </S.FollowerImgBox>
-          <S.FollowerTextBox>
+          <S.FollowerTextBox onClick={() => navigate('/profile')}>
             <S.FollowerTitleContent>애월읍 한라봉 최고 맛집</S.FollowerTitleContent>
             <S.FollowerIntroContent>정성을 다해 농사짓는 한라봉</S.FollowerIntroContent>
           </S.FollowerTextBox>
-          <S.FollowerButton>팔로우</S.FollowerButton>
-        </S.FollowerBox>
-        <S.FollowerBox>
-          <S.FollowerImgBox>
-            <img src={basicProfile} alt='' />
-          </S.FollowerImgBox>
-          <S.FollowerTextBox>
-            <S.FollowerTitleContent>애월읍 한라봉 최고 맛집</S.FollowerTitleContent>
-            <S.FollowerIntroContent>정성을 다해 농사짓는 한라봉</S.FollowerIntroContent>
-          </S.FollowerTextBox>
-          <S.FollowerPauseButton>취소</S.FollowerPauseButton>
+          {flag ? (
+            <S.FollowerButton onClick={() => setFlag(!flag)}>팔로우</S.FollowerButton>
+          ) : (
+            <S.FollowerPauseButton onClick={() => setFlag(!flag)}>취소</S.FollowerPauseButton>
+          )}
         </S.FollowerBox>
       </S.FollowerRowBox>
     </S.ProfileFollowersLayout>
