eae67ddd686e2cdc3911afe78915a6b416332876
kirnjiyun <kimjiyunee@naver.com>
2023-10-25 19:02:06 +0900

    feat(검색) : 검색 기능 구현(#65)

--- src/pages/home/search/SearchPage.jsx
+++ src/pages/home/search/SearchPage.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TabMenu from '../../../components/common/TabMenu/TabMenu';
 import SearchHeader from '../../../components/home/search/SearchHeader';
 import Search from '../../../components/home/search/Search';
 import * as S from './searchPage.styled';
 
 export default function SearchPage() {
+  const [searchResults, setSearchResults] = useState([]);
+
   return (
     <S.HomeLayout>
-      <SearchHeader />
-      <Search />
+      <SearchHeader setSearchResults={setSearchResults} />
+      <Search searchResults={searchResults} />
       <TabMenu />
     </S.HomeLayout>
   );
