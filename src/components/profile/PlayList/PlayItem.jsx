c88d00381d4c7cf1b4308507a4993e67b0634fe2
esoby <141106@naver.com>
2023-10-30 14:24:32 +0900

    feat(프로필) : 상품 게시글 (좋아하는 글) 불러오기 (#104)

--- src/components/profile/PlayList/PlayItem.jsx
+++ src/components/profile/PlayList/PlayItem.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import * as S from './PlayItem.styled';
+import { useParams, useNavigate } from 'react-router-dom';
 
-export default function PlayItem({ img, title, userId }) {
+export default function PlayItem({ postId, img, title, userId }) {
+  const navigate = useNavigate();
   return (
-    <S.ItemBox>
+    <S.ItemBox onClick={() => navigate('/post/' + postId)}>
       <S.ItemImg src={img} />
       <S.ItemTitleContent>{title}</S.ItemTitleContent>
-      <S.ItemIDContent>{userId}</S.ItemIDContent>
+      <S.ItemIDContent>{'@' + userId}</S.ItemIDContent>
     </S.ItemBox>
   );
 }
