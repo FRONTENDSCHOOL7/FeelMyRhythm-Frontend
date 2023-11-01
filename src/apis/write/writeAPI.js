b77f543d46450329df6aed843d38502daf6dc52c
Byeong Min Cho <merrybmc@gmail.com>
2023-11-02 03:53:02 +0900

    feat(게시글 삭제) : 게시글 삭제 기능 구현 (#189)

--- src/apis/write/writeAPI.js
+++ src/apis/write/writeAPI.js
@@ -1,12 +1,19 @@
 import { api } from '../baseURL';
 
+// 게시글 생성
 export const createPost = async (content) => {
   const { data } = await api.post('/post', content);
   return data;
 };
 
+// 게시글 수정
 export const updatePost = async ({ id, postContent }) => {
-  console.log('통신', id, postContent);
   const { data } = await api.put(`/post/${id}`, postContent);
+  return data;
+};
+
+// 게시글 삭제
+export const deletePost = async (id) => {
+  const { data } = await api.delete(`/post/${id}`);
   return data;
 };
