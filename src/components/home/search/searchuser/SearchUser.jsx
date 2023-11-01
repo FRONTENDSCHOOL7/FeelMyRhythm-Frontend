98a7b7d55474640b6b059648131049a62ca3abe6
kirnjiyun <kimjiyunee@naver.com>
2023-11-01 14:01:36 +0900

    feat(계정검색) : 우리 조의 계정만 나오게 필터링 (#160)

--- src/components/home/search/searchuser/SearchUser.jsx
+++ src/components/home/search/searchuser/SearchUser.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import * as S from './userProfile.styled';
 import SearchUserProfile from './SearchUserProfile';
-
 export default function Search({ searchResults }) {
-  const accountname = searchResults.map((profile) => profile.accountname);
   return (
     <S.SearchLayout>
-      {searchResults.map((profile, index) => (
-        <SearchUserProfile
-          key={index}
-          src={profile.image}
-          alt='profile'
-          username={profile.username}
-          accountname={profile.accountname}
-        />
-      ))}
+      {searchResults
+        .filter((profile) => String(profile.intro).split('🈳')[0] === 'ms7-3')
+        .map((profile, index) => (
+          <SearchUserProfile
+            key={index}
+            src={profile.image}
+            alt='profile'
+            username={profile.username}
+            accountname={profile.accountname}
+          />
+        ))}
     </S.SearchLayout>
   );
 }
