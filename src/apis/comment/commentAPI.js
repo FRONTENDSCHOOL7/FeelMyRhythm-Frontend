166216d5e24f7fb71250c0e9e1caafdba38b5aa1
Jeong-a.Kim <somdari0423@gmail.com>
2023-11-04 01:31:08 +0900

    feature : 상세게시글 댓글 목록/작성 api 파일추가 (#219)

--- src/apis/comment/commentAPI.js
+++ src/apis/comment/commentAPI.js
@@ -15,17 +15,14 @@ export const commentListAPI = async (postid) => {
   return result.data;
 };
 
-//댓글 작성
-// export const commentWriteAPI = async ({ postid, comment }) => {
-//   console.log(postid);
-//   console.log(comment);
-//   const response = await api.post(`/post/${postid}/comments`, comment);
+//댓글 삭제
+export const commentDeleteAPI = async (postid, commentid) => {
+  const result = await api.delete(`post/${postid}/comments/${commentid}`);
+  return result.data;
+};
 
-//   return response.data;
-// };
-
-// //댓글 목록
-// export const commentListAPI = async (postid) => {
-//   const result = await api.get(`/post/${postid}/comments`);
-//   return result.data;
-// };
+//댓글 신고
+export const commentReportAPI = async (postid, commentid) => {
+  const result = await api.post(`post/${postid}/comments/${commentid}/report`);
+  return result.data.comment;
+};
