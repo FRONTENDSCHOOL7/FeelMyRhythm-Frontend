1fea37c33b0a304156f9e339215b2397c1cae2e6
kirnjiyun <kimjiyunee@naver.com>
2023-11-04 22:31:01 +0900

    fix : 팔로우 상태일때 게시글 클릭하면 해당 게시글 페이지로 (#226)

--- src/components/home/EntirePosts.jsx
+++ src/components/home/EntirePosts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { showEntirePosts } from '../../apis/home/entirePosts';
 import UserProfile from './UserProfile';
