22f17eea7685e174ad4021bfc7591f8ae80ff22d
esoby <141106@naver.com>
2023-11-05 18:41:22 +0900

    feat(loading) : 홈 / 프로필 페이지 로딩 중 처리 (#246)

---
+++ src/components/common/Loading/Loading.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import { MoonLoader } from 'react-spinners'; //설치한 cliploader을 import한다
+
+export default function Loading() {
+  return (
+    <Box>
+      <MoonLoader color='#7B86AA' size={60} />
+    </Box>
+  );
+}
+const Box = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 99999;
+  background-color: #f8f9fc;
+`;
--- src/components/profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/profile/ProfileInfo/ProfileInfo.jsx
@@ -8,32 +8,40 @@ import { readAccountInfo } from '../../../apis/profile/accountInfoAPI';
 import { followUser } from '../../../apis/profile/followAPI';
 import { unfollowUser } from '../../../apis/profile/unfollowAPI';
 import { useMutation, useQuery } from '@tanstack/react-query';
+import Loading from '../../common/Loading/Loading';
 
 export default function ProfileInfo() {
   const user = useRecoilValue(atomMyInfo);
   const navigate = useNavigate();
   const { accountname } = useParams();
-  const [userFlag, setUserFlag] = useState(false);
   const [profile, setProfile] = useState({});
   const [follow, setFollow] = useState(profile.isfollow);
   const [followerCnt, setFollowerCnt] = useState(0);
   const [followingCnt, setFollowingCnt] = useState(0);
 
+  let [loading, setLoading] = useState(false);
+
   const { data, error } = useQuery({
     queryFn: () =>
       readAccountInfo(accountname).then((res) => {
-        setUserFlag(user.accountname === accountname);
+        // setUserFlag(user.accountname === accountname);
         setProfile(res.profile);
         setFollowerCnt(res.profile.followerCount);
         setFollowingCnt(res.profile.followingCount);
-        return res;
+        return res.profile;
       }),
     queryKey: [accountname, user?.accountname]
   });
 
-  const handleFollowClick = (url) => {
-    navigate('/profile/' + profile.accountname + url);
-  };
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setLoading(true);
+    }, 1000);
+  });
+
+  useEffect(() => {
+    setFollow(profile.isfollow);
+  }, [profile.isfollow]);
 
   const followMutation = useMutation({
     mutationFn: followUser,
@@ -43,7 +51,6 @@ export default function ProfileInfo() {
     },
     onError: (err) => {}
   });
-
   const unfollowMutation = useMutation({
     mutationFn: unfollowUser,
     onSuccess: (res) => {
@@ -53,65 +60,71 @@ export default function ProfileInfo() {
     onError: (err) => {}
   });
 
-  useEffect(() => {
-    setFollow(profile.isfollow);
-  }, [profile.isfollow]);
-
+  const handleFollowClick = (url) => {
+    navigate('/profile/' + profile.accountname + url);
+  };
   const onNavigateProfileUpdate = () => {
     navigate('/profile/update');
   };
 
+  if (data && loading)
+    return (
+      <S.ProfileInfoLayout>
+        <S.RowBox>
+          <S.ColBox>
+            <S.FollowNum onClick={() => handleFollowClick('/follower')} $fontColor='#000'>
+              {followerCnt}
+            </S.FollowNum>
+            <S.FollowText>followers</S.FollowText>
+          </S.ColBox>
+          <S.ImgBox>
+            <img
+              src={!profile.image || String(profile.image).includes('Ellipse.png') ? basicProfile : profile.image}
+              alt=''></img>
+          </S.ImgBox>
+          <S.ColBox>
+            <S.FollowNum onClick={() => handleFollowClick('/following')} $fontColor='#767676'>
+              {followingCnt}
+            </S.FollowNum>
+            <S.FollowText>followings</S.FollowText>
+          </S.ColBox>
+        </S.RowBox>
+        <S.TitleContent>{profile.username}</S.TitleContent>
+        <S.IDContent>{'@' + profile.accountname}</S.IDContent>
+        {String(profile.intro).split('ms7-3🈳')[1] && (
+          <S.IntroContent>{String(profile.intro).split('ms7-3🈳')[1]}</S.IntroContent>
+        )}
+        <S.RowButtonBox>
+          {user.accountname === accountname && (
+            <S.LinkButton $width='120px' onClick={onNavigateProfileUpdate}>
+              프로필 수정
+            </S.LinkButton>
+          )}
+          {!(user.accountname === accountname) && follow && (
+            <S.FollowButton
+              $follow=''
+              onClick={() => {
+                unfollowMutation.mutate(profile.accountname);
+              }}>
+              언팔로우
+            </S.FollowButton>
+          )}
+          {!(user.accountname === accountname) && !follow && (
+            <S.FollowButton
+              $follow='true'
+              onClick={() => {
+                followMutation.mutate(profile.accountname);
+              }}>
+              팔로우
+            </S.FollowButton>
+          )}
+        </S.RowButtonBox>
+      </S.ProfileInfoLayout>
+    );
+
   return (
-    <S.ProfileInfoLayout>
-      <S.RowBox>
-        <S.ColBox>
-          <S.FollowNum onClick={() => handleFollowClick('/follower')} $fontColor='#000'>
-            {followerCnt}
-          </S.FollowNum>
-          <S.FollowText>followers</S.FollowText>
-        </S.ColBox>
-        <S.ImgBox>
-          <img
-            src={!profile.image || String(profile.image).includes('Ellipse.png') ? basicProfile : profile.image}
-            alt=''></img>
-        </S.ImgBox>
-        <S.ColBox>
-          <S.FollowNum onClick={() => handleFollowClick('/following')} $fontColor='#767676'>
-            {followingCnt}
-          </S.FollowNum>
-          <S.FollowText>followings</S.FollowText>
-        </S.ColBox>
-      </S.RowBox>
-      <S.TitleContent>{profile.username}</S.TitleContent>
-      <S.IDContent>{'@' + profile.accountname}</S.IDContent>
-      {String(profile.intro).split('ms7-3🈳')[1] && (
-        <S.IntroContent>{String(profile.intro).split('ms7-3🈳')[1]}</S.IntroContent>
-      )}
-      <S.RowButtonBox>
-        {userFlag && (
-          <S.LinkButton $width='120px' onClick={onNavigateProfileUpdate}>
-            프로필 수정
-          </S.LinkButton>
-        )}
-        {!userFlag && follow && (
-          <S.FollowButton
-            $follow=''
-            onClick={() => {
-              unfollowMutation.mutate(profile.accountname);
-            }}>
-            언팔로우
-          </S.FollowButton>
-        )}
-        {!userFlag && !follow && (
-          <S.FollowButton
-            $follow='true'
-            onClick={() => {
-              followMutation.mutate(profile.accountname);
-            }}>
-            팔로우
-          </S.FollowButton>
-        )}
-      </S.RowButtonBox>
-    </S.ProfileInfoLayout>
+    <>
+      <Loading />
+    </>
   );
 }
