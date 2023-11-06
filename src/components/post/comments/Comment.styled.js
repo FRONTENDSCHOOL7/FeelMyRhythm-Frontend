dfceef6b66ebf148058483606f5294355cabcfab
kirnjiyun <kimjiyunee@naver.com>
2023-11-06 23:43:01 +0900

    fix: 이상한 부분 삭제 (#268)

--- src/components/post/comments/Comment.styled.js
+++ src/components/post/comments/Comment.styled.js
@@ -37,7 +37,6 @@ export const TitleContent = styled.p`
   padding-top: 3px;
   font-weight: 500;
   margin-left: 3px;
-  background-color: green;
 `;
 
 export const TimeBox = styled.div`
@@ -45,7 +44,6 @@ export const TimeBox = styled.div`
   color: ${({ theme }) => theme.subTextColor};
   padding-left: 5px;
   padding-top: 7px;
-  background-color: yellow;
 `;
 
 export const MoreBtn = styled.button`
