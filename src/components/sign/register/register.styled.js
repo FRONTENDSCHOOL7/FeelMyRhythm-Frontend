77d02b5637ce5d46826e75d776e0b4b55a7e5eb6
Byeong Min Cho <merrybmc@gmail.com>
2024-07-09 23:55:36 +0900

    feat : 이메일 입력 (#323)

--- src/components/sign/register/register.styled.js
+++ src/components/sign/register/register.styled.js
@@ -39,3 +39,79 @@ export const ChangeImg = styled.img`
   height: 20px;
   cursor: pointer;
 `;
+
+export const InputBox = styled.div`
+  border-bottom: 1px solid #dbdbdb;
+`;
+
+export const SubContent = styled.p`
+  color: ${({ theme }) => theme.subTextColor};
+
+  font-size: 12px;
+  font-weight: 500;
+
+  padding-bottom: 10px;
+`;
+
+export const Input = styled.input`
+  width: 70%;
+  height: 32px;
+
+  padding: 0;
+  border: none;
+  outline: none;
+
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 14px;
+
+  margin-bottom: 8px;
+
+  padding-left: 8px;
+
+  background-color: ${({ theme }) => theme.backgroundColor};
+  color: ${({ theme }) => theme.textColor};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.subTextColor};
+  }
+`;
+
+export const CheckEmailButton = styled.button`
+  width: 70px;
+  height: 32px;
+  border: none;
+  color: ${({ theme }) => theme.btnTextColor};
+  background-color: ${({ valid, theme }) => (valid === 'success' ? 'transparent' : theme.btnEnabledBgColor)};
+
+  font-size: ${({ valid }) => (valid === 'success' ? '16px' : '12px')};
+  /* color: ${({ valid }) => (valid === 'success' ? 'green' : '#767676')}; */
+  font-weight: 500;
+
+  margin-bottom: ${({ valid }) => (valid === 'success' ? '5px' : '8px')};
+  margin-left: auto;
+
+  text-align: ${({ valid }) => (valid === 'success' ? 'right' : 'center')};
+  /* border: ${({ valid }) => (valid === 'success' ? 'none' : '1px solid #dbdbdb')}; */
+
+  cursor: ${({ valid }) => (valid === 'success' ? 'default' : 'pointer')};
+  border-radius: 30px;
+`;
+
+export const CheckBox = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const WarningContent = styled.p`
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 14px;
+
+  padding-top: 6px;
+  padding-bottom: 16px;
+`;
+
+export const EmailWarningContent = styled(WarningContent)`
+  color: ${({ valid }) => (valid === '사용 가능한 이메일 입니다.' ? 'green' : '#eb5757')};
+`;
--- src/pages/sign/register/RegisterPage.jsx
+++ src/pages/sign/register/RegisterPage.jsx
@@ -1,14 +1,26 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import * as S from './registerPage.styled';
 import Title from '../../../components/sign/register/Title';
 import ProfileImage from '../../../components/sign/register/ProfileImage';
+import EmailInput from '../../../components/sign/register/EmailInput';
 export default function RegisterPage() {
   const [image, setImage] = useState('');
+
+  const [email, setEmail] = useState('');
+  const emailRef = useRef(null);
+  const [emailValidState, setEmailValidState] = useState('');
 
   return (
     <S.RegisterLayout>
       <Title />
       <ProfileImage setImage={setImage} />
+      <EmailInput
+        email={email}
+        emailRef={emailRef}
+        setEmail={setEmail}
+        emailValidState={emailValidState}
+        setEmailValidState={setEmailValidState}
+      />
     </S.RegisterLayout>
   );
 }
